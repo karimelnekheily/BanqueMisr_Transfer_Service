8f8f1c42273187192cccb22cd5771a37c52466cb
karim Elnekheily <elnekheilykarim@gmail.com>
2024-09-11 14:55:45 +0300

    changing port

--- src/main/java/com/example/MoneyTransfer/config/CrosConfig.java
+++ src/main/java/com/example/MoneyTransfer/config/CrosConfig.java
@@ -1,5 +1,6 @@
 package com.example.MoneyTransfer.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
